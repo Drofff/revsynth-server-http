e4b2929307de5d74b1064e22c25951fa2fa1254f
Mykhailo Palahuta <mykhailo.palahuta@care.com>
2023-04-20 00:36:30 +0300

    allow all cors

--- handler/handler.go
+++ handler/handler.go
@@ -9,7 +9,31 @@ import (
 	"github.com/Drofff/revsynth-server-http/service"
 )
 
+const (
+	headerAllowOrigin  = "Access-Control-Allow-Origin"
+	headerAllowMethods = "Access-Control-Allow-Methods"
+	headerAllowHeaders = "Access-Control-Allow-Headers"
+	headerContentType  = "Content-Type"
+
+	allowAll = "*"
+
+	contentTypeJSON = "application/json"
+)
+
+func addCORSHeadersPOST(w http.ResponseWriter) {
+	w.Header().Add(headerAllowOrigin, allowAll)
+	w.Header().Add(headerAllowMethods, http.MethodPost)
+	w.Header().Add(headerAllowHeaders, allowAll)
+}
+
 func HandleRequest(w http.ResponseWriter, req *http.Request) {
+	addCORSHeadersPOST(w)
+
+	if req.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
 	if req.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
@@ -25,12 +49,14 @@ func HandleRequest(w http.ResponseWriter, req *http.Request) {
 	in := &service.SynthesiseInput{}
 	err = json.Unmarshal(data, in)
 	if err != nil {
+		log.Printf("WARN: marshal error: %v", err.Error())
+
 		w.WriteHeader(http.StatusBadRequest)
-		_, err = w.Write([]byte("invalid input format"))
+		_, err = w.Write([]byte("{\"msg\":\"invalid input format\"}"))
 		if err != nil {
 			log.Println(err)
-			return
 		}
+		return
 	}
 
 	out, err := service.Synthesise(in)
@@ -39,8 +65,8 @@ func HandleRequest(w http.ResponseWriter, req *http.Request) {
 		_, err = w.Write([]byte(err.Error()))
 		if err != nil {
 			log.Println(err)
-			return
 		}
+		return
 	}
 
 	outJSON, err := json.Marshal(out)
@@ -50,6 +76,7 @@ func HandleRequest(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	w.Header().Add(headerContentType, contentTypeJSON)
 	_, err = w.Write(outJSON)
 	if err != nil {
 		log.Println(err)
